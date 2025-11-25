889e9a191e476b4abaf786946720916da71b9dc8
ahihi <maimaichamcom.com>
2025-11-25 14:48:06 +0700

    add enemy class

---
+++ swing/source/main/components/characters/Enemy.java
@@ -0,0 +1,55 @@
+package components.characters;
+
+/**
+ * Enemy - Character with AI behavior
+ * Doesn't have health (can't be killed)
+ * Kills player instantly on contact
+ */
+public class Enemy extends Character {
+    private int detectionRange;
+    
+    public Enemy(int x, int y) {
+        super(x, y, 32, 32, "enemy");
+        this.detectionRange = 100;
+    }
+    
+    @Override
+    protected String getDefaultState() {
+        return "patrol";
+    }
+    
+    @Override
+    protected int getDefaultSpeed() {
+        return 2;
+    }
+    
+    @Override
+    public void update() {
+        // AI behavior: patrol, chase player
+        if (state.equals("patrol")) {
+            patrol();
+        } else if (state.equals("chase")) {
+            chasePlayer();
+        }
+    }
+    
+    private void patrol() {
+        // Simple patrol logic
+        moveRight();
+    }
+    
+    private void chasePlayer() {
+        // Chase player logic
+    }
+    
+    /**
+     * Called when enemy touches player
+     */
+    public void onContactWithPlayer(Player player) {
+        player.die();
+    }
+    
+    public int getDetectionRange() {
+        return detectionRange;
+    }
+}
