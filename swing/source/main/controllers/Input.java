4570ba04b95ec2fb31b8b9a52006101016b2e761
billy2204 <billy220406@gmail.com>
2025-11-26 22:23:22 +0700

    add Input class to store key event

---
+++ swing/source/main/controllers/Input.java
@@ -0,0 +1,27 @@
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+public class Input implements KeyListener {
+    private boolean[] press ; 
+
+    public Input() {
+        press = new boolean[255];
+    }
+
+    public boolean isPressed(int keyCode) {
+        return press[keyCode];  
+    }
+    public void keyPressed(KeyEvent e) {
+        press[e.getKeyCode()] = true;
+    }
+    public void keyReleased(KeyEvent e) {
+        press[e.getKeyCode()] = false;
+       
+    }
+    public void keyTyped(KeyEvent e) {
+
+    }
+    public static void main(String[] args) {
+    }
+
+}
