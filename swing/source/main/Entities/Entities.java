731fe4f4c70760eb99cb78ce02d6d1c6e9d26c9b
billy2204 <billy220406l@gmail.com>
2025-12-02 14:48:04 +0700

    modified Entities

--- swing/source/main/Entities/Entities.java
+++ swing/source/main/Entities/Entities.java
@@ -1,21 +1,33 @@
 package Entities;
 
-import graphics.Renderable;
-
-public class Entities implements Renderable  {
-    @Override
-    public void render(java.awt.Graphics2D g2) {
-        // Implementation here
-    }
-
-    @Override
-    public boolean isVisible() {
-        return false;
-    }
-
-    @Override
-    public int getLayer() {
-        return 0;
-    }
+public abstract class Entities  {
     
+    private int x; // coordinate x
+    private int y; // coordinate y
+    private final String type;
+    protected String state; // Trạng thái hiện tại (idle, open, etc.)
+
+    public Entities(String type, int x, int y) { // new Entities("type", x, y)
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.state = "idle";
+    }
+    public Entities(String type, String state, int x, int y) { // new Entities("type", " run", x ,y ),
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.state = state;
+    }
+
+    // Getters
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public String getType() { return type; }
+    public String getState() { return state; }
+    // Setters
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+    public void setState(String state) { this.state = state; }
+
 }
